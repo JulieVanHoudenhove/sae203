8332e98e1e8f62f6103610a831226198b18cddc5
JulieVanHoudenhove <julie.van-houdenhove@etudiant.univ-reims.fr>
2022-03-04 16:03:15 +0100

    commit

--- index.php
+++ index.php
@@ -10,12 +10,19 @@
     </section>
     <section class="sortie">
         <p>Les dernières sorties</p>
-        <div>
+        <div class="ordi">
             <a href="listing.php#16"><img src="images/arcane.jpg" alt="Affiche d'Arcane"></a>
             <a href="listing.php#19"><img src="images/squid_game.jpg" alt="Affiche de Squid Game"></a>
             <a href="listing.php#38"><img src="images/shadow_and_bone.jpg" alt="Affiche de Shadow and Bone : La saga Grisha"></a>
             <a href="listing.php#33"><img src="images/la_voie_du_tablier.jpg" alt="Affiche de La Voie du tablier"></a>
             <a href="listing.php#6"><img src="images/lupin.jpg" alt="Affiche de Lupin"></a>
+        </div>
+        <div class="mobil">
+            <a href="listing.php#17"><img src="images/arcane.jpg" alt="Affiche d'Arcane"></a>
+            <a href="listing.php#20"><img src="images/squid_game.jpg" alt="Affiche de Squid Game"></a>
+            <a href="listing.php#39"><img src="images/shadow_and_bone.jpg" alt="Affiche de Shadow and Bone : La saga Grisha"></a>
+            <a href="listing.php#34"><img src="images/la_voie_du_tablier.jpg" alt="Affiche de La Voie du tablier"></a>
+            <a href="listing.php#7"><img src="images/lupin.jpg" alt="Affiche de Lupin"></a>
         </div>
     </section>
 </main>
